e924a677b6f7af804431b2cbbb2545d20ac764a1
acrowe1 <113642049+acrowe1@users.noreply.github.com>
2024-03-27 16:24:46 -0500

    Update PlayerControls.cs
    - Added sprint functionality 
    - Added random coin generation (in a radius around the player)

--- PlayerControls.cs
+++ PlayerControls.cs
@@ -1,5 +1,3 @@
-// only uses keyboard controlls atm (W,A,S,D)
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,47 +7,96 @@ public class PlayerControls : MonoBehaviour
 {
     private List<string> inventory = new List<string>();
     public float movementSpeed = 5.0F;
+    public float sprintSpeedMultiplier = 2.0F; // Multiplier for sprint speed
     public float jumpForce = 10.0F;
     private Rigidbody2D rb;
     private bool isPaused = false;
     private bool isGrounded = true; 
-    public GameObject coinPrefab; 
+    public GameObject Coin; 
+    public float coinSpawnRadius = 5.0F; // Radius within which coins will spawn
+
+void Start()
+{
+    rb = GetComponent<Rigidbody2D>();
+    rb.gravityScale = 0;
+
+    List<Vector3> spawnedCoinPositions = new List<Vector3>(); 
+
+    float randomNumber = Random.Range(0f, 10f); 
 
 
-    void Start()
+    // Generate random coin spawn points within a radius around the player
+    for (int i = 0; i < randomNumber; i++) 
     {
-        rb = GetComponent<Rigidbody2D>();
-        rb.gravityScale = 0; 
+        Vector2 randomSpawnPoint = Random.insideUnitCircle * coinSpawnRadius;
+        Vector3 spawnPosition = new Vector3(transform.position.x + randomSpawnPoint.x, transform.position.y + randomSpawnPoint.y, 0);
+
+        // Check if the spawn position is already occupied by another coin
+        while (IsPositionOccupied(spawnPosition, spawnedCoinPositions))
+        {
+            randomSpawnPoint = Random.insideUnitCircle * coinSpawnRadius;
+            spawnPosition = new Vector3(transform.position.x + randomSpawnPoint.x, transform.position.y + randomSpawnPoint.y, 0);
+        }
+
+        GameObject newCoin = Instantiate(Coin, spawnPosition, Quaternion.identity);
+        // Assign "Coin" tag to the instantiated coin object
+        newCoin.tag = "Coin"; 
+        spawnedCoinPositions.Add(spawnPosition); 
     }
+}
+
+// Method to check if a position is already occupied by another coin
+bool IsPositionOccupied(Vector3 position, List<Vector3> occupiedPositions)
+{
+    foreach (Vector3 occupiedPosition in occupiedPositions)
+    {
+        if (Vector3.Distance(position, occupiedPosition) < 1.0f) 
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+
 
     void Update()
     {
         // I'm working on a rough pause menu 
         if (!isPaused) 
         {
-            if (Input.GetKey(KeyCode.A)) {
-                transform.position += Vector3.left * Time.deltaTime * movementSpeed;
-            } else if (Input.GetKey(KeyCode.D)) {
-                transform.position += Vector3.right * Time.deltaTime * movementSpeed;
-            } else if (Input.GetKey(KeyCode.W)) {
-                transform.position += Vector3.up * Time.deltaTime * movementSpeed; 
-            } else if (Input.GetKey(KeyCode.S)) {
-                transform.position += Vector3.down * Time.deltaTime * movementSpeed; 
-            }
+            // Controller controls
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
 
+            // hold down left shift/'A' to sprint     
+            bool isSprinting = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.JoystickButton0); 
+            float currentSpeed = isSprinting ? movementSpeed * sprintSpeedMultiplier : movementSpeed;
+
+            Vector3 movement = new Vector3(horizontalInput, verticalInput, 0);
+            transform.position += movement * Time.deltaTime * currentSpeed;
         }
     }
 
-
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Coin")) {
-            // Add the item to the inventory list
+        if (collision.gameObject.CompareTag("Coin")) 
+        {
+            // Add the item to the inventory list & destroy after
             string itemName = collision.gameObject.name;
             inventory.Add(itemName);
-
-            // Destroy item after picking it up
+            PrintInventory();
             Destroy(collision.gameObject);
+        }
+    }
+
+    // print inventory list to console
+    void PrintInventory()
+    {
+        Debug.Log("Inventory:");
+        foreach (string item in inventory)
+        {
+            Debug.Log("- " + item);
         }
     }
 }
